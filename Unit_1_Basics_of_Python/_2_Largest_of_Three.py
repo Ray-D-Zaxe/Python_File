c0fb44644d08e263fa0d2e5d72747abe0978f527
Ray <ray.dreamer400@gmail.com>
2025-06-01 17:52:55 +0530

    Unit1 and Unit2

---
+++ Unit_1_Basics_of_Python/_2_Largest_of_Three.py
@@ -0,0 +1,17 @@
+a = 5
+b, c = 8, 2
+
+if a > b and a > c:
+    print("a is the largest number:", a)
+elif b > a and b > c:
+    print("b is the largest number:", b)
+else:
+    print("c is the largest number:", c)
+
+
+
+#######################################################################################
+'''
+Output:
+b is the largest number: 8
+'''
